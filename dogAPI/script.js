3206be12a45a88549f52b994f52d47cf2660954e
Efoneli <abbaefoneli@gmail.com>
2023-01-30 07:44:17 +1030

    promises

--- dogAPI/script.js
+++ dogAPI/script.js
@@ -5,13 +5,22 @@
 const dogImageDiv = document.getElementById('dogImage')
 const dogButton = document.getElementById('dogButton')
 
-const getNewDog = () => {
-fetch('https://dog.ceo/api/breeds/image/random')
-    .then(response => response.json())
-    .then(json => {
-            // console.log(json.message)
-            dogImageDiv.innerHTML = `<img src="${json.message}" height=300 width=300 alt="dog" />`
-        })
+// const getNewDog = () => {
+// fetch('https://dog.ceo/api/breeds/image/random')
+//     .then(response => response.json())
+//     .then(json => {
+//             // console.log(json.message)
+//             dogImageDiv.innerHTML = `<img src="${json.message}" height=300 width=300 alt="dog" />`
+//         })
+//     }
+
+    // dogButton.onclick = () => getNewDog()
+
+    const getDog = async () => {
+        const url = 'https://dog.ceo/api/breeds/image/random'
+        const response = await fetch(url)
+        const data = await response.json()
+        console.log(data)
     }
 
-    dogButton.onclick = () => getNewDog()
+    getDog()
---
+++ promises/script.js
@@ -0,0 +1,16 @@
+// const getSoup = async () => {
+//     const url = ''
+//     const response = await fetch(url)
+//     const data = await response.json()
+//     console.log(data)
+// }
+
+// getSoup()
+
+
+const getSoup = async () => {
+    const soup = promise1
+    console.log(soup)
+}
+
+getSoup()
