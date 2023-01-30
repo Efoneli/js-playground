fe0cd926941c9b7259e176d3fdb97d2f03e853da
Efoneli <abbaefoneli@gmail.com>
2023-01-31 07:51:07 +1030

    promise

--- promises/script.js
+++ promises/script.js
@@ -8,9 +8,21 @@
 // getSoup()
 
 
-const getSoup = async () => {
-    const soup = promise1
-    console.log(soup)
+// const getSoup = async () => {
+//     const soup = promise1
+//     console.log(soup)
+// }
+
+// getSoup()
+
+// 1. create a function
+// 2. use async 
+// 3. use await
+const getDog = async () => {
+    const url = 'https://dog.ceo/api/breeds/image/random'
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data)
 }
 
-getSoup()
+getDog()
