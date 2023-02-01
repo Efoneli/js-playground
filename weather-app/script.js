f6b954725a109c1dbd3beb4abb30876f5a538f4c
Efoneli <abbaefoneli@gmail.com>
2023-02-01 13:06:00 +1030

    test

--- weather-app/script.js
+++ weather-app/script.js
@@ -1,3 +1,20 @@
+
+ const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '30d6f31d29msh80d0e602bfcb291p10394ajsnd9fc5b2e08a2',
+		'X-RapidAPI-Host': 'open-weather-map27.p.rapidapi.com'
+	}
+};
+
+fetch('https://open-weather-map27.p.rapidapi.com/weather', options)
+	.then(response => response.json())
+	.then(data => console.log(data.name))
+	.catch(err => console.error(err));
+ 
+
+
+
 /**
  * Weather App
  * TODO: Complete getWeatherData() to return json response Promise
@@ -13,8 +30,6 @@
 "max-temp"
 */
 
-// API_KEY for maps api
-let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
 
 /**
  * Retrieve weather data from openweathermap
@@ -22,7 +37,7 @@ let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
  * HINT: URL should look like this: 
  * https://api.openweathermap.org/data/2.5/weather?q=detroit&appid=a8e71c9932b20c4ceb0aed183e6a83bb&units=imperial
  */
-getWeatherData = (city) => {
+const getWeatherData = (city) => {
   const URL = "https://api.openweathermap.org/data/2.5/weather";
   //HINT: Use template literals to create a url with input and an API key
 
@@ -45,6 +60,7 @@ const searchCity = () => {
  */
 const showWeatherData = (weatherData) => {
   //CODE GOES HERE
+  const response = JSON.stringify(weatherData)
   
 }
 
