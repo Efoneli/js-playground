3abc89e68c6fc623f9b9e912738b2feacb3153f5
Efoneli <abbaefoneli@gmail.com>
2023-02-01 15:01:43 +1030

    classes

--- classes/script.js
+++ classes/script.js
@@ -1,22 +1,38 @@
 class Car {
-    constructor(name, color, speed) {
+    constructor(name, color, topSpeed, currentSpeed) {
         // properties
         this.name = name
         this.color = color
-        this.speed = speed
+        this.topSpeed = topSpeed
+        this.currentSpeed = 0
+    }
+
+    zeroToSixty () {
+        setTimeout(() => {
+            console.log('phew!!, that was fast')
+            this.currentSpeed = 50
+        })
     }
 
     drive() {
-        console.log('two miles')
+        // console.log('two miles')
+        this.currentSpeed += 10
+        console.log(`driving speed at ${this.currentSpeed}`)
     }
 
     brake() {
-        console.log('brakes in')
+        // console.log('brakes in')
     }
 }
 
 const ferrari = new Car('ferrari','red', '250')
-console.log(ferrari)
+// console.log(ferrari)
+
+console.log(ferrari.currentSpeed)
 
 ferrari.drive()
 ferrari.brake()
+
+console.log(ferrari.currentSpeed)
+
+ferrari.zeroToSixty()
